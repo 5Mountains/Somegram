b12fda450d5bbcc0ee1f3d06b97ef921c1741f56
vlad.piatyhor <vladyslav.piatyhor@customertimes.com>
2022-07-12 19:15:34 +0300

    add basic layout

---
+++ src/screens/PostUploadScreen/PostUploadScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {styles} from './styles';
+
+export const PostUploadScreen = () => {
+  return (
+    <View style={styles.root}>
+      <Text>PostUploadScreen</Text>
+    </View>
+  );
+};
---
+++ src/screens/PostUploadScreen/index.ts
@@ -0,0 +1,1 @@
+export {PostUploadScreen} from './PostUploadScreen';
